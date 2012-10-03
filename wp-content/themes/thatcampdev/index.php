886915233b642f054c3495101d76185ea39c36ee
Boone B Gorges <boonebgorges@gmail.com>
2012-10-03 11:29:47 -0400

    Improving Group/Camp linking:
    - Mods index.php of theme to use a bp_has_groups() loop (first pass)
    - Adds THATCamp Setup admin screen, with datepicker
    - Adds functions to display actual date in loops

--- wp-content/themes/thatcampdev/index.php
+++ wp-content/themes/thatcampdev/index.php
@@ -11,11 +11,22 @@
 	<div id="content" class="clearfix" role="main">
 		<div id="upcoming-camps" class="clearfix feature-box">
 			<h2>Upcoming THATCamps</h2>
+
+			<?php if ( bp_has_groups() ) : while ( bp_groups() ) : bp_the_group() ?>
+
 			<article class="camp-listitem">
-				<div class="camp-listdate">August 10th 2012</div>
-				<h3><a href="" class="camplink">THATCamp title goes here as a link</a></h3>
-				<p class="camp-listmeta">Workshops <span class="camp-listnegative">no</span></p>
+				<div class="camp-listdate"><?php thatcamp_camp_date() ?></div>
+				<h3><a href="<?php thatcamp_camp_permalink() ?>" class="camplink"><?php bp_group_name() ?></a></h3>
+				<p class="camp-listmeta">Workshops
+					<?php if ( thatcamp_camp_has_workshops() ) : ?>
+						<span class="camp-listpositive">yes</span></p>
+					<?php else : ?>
+						<span class="camp-listnegative">no</span></p>
+					<?php endif ?>
 			</article>
+
+			<?php endwhile; endif ?>
+
 			<article class="camp-listitem">
 				<div class="camp-listdate">August 10th 2012</div>
 				<h3><a href="" class="camplink">THATCamp title goes here as a link</a></h3>
