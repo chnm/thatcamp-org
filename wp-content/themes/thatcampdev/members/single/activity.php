42a463da5266ad416c7e0b800014e184e55eaf5c
Tammie Lister <karmatosed@gmail.com>
2012-10-24 11:29:02 +0100

    What's new form

--- wp-content/themes/thatcampdev/members/single/activity.php
+++ wp-content/themes/thatcampdev/members/single/activity.php
@@ -11,11 +11,21 @@
 
 <?php do_action( 'bp_before_member_activity_post_form' ); ?>
 
-<?php
-//if ( is_user_logged_in() && bp_is_my_profile() && ( !bp_current_action() || bp_is_current_action( 'just-me' ) ) )
-//	locate_template( array( 'activity/post-form.php'), true );
+<div id="item-header">
+	<span class="user-nicename">@<?php bp_displayed_user_username(); ?></span>
+</div>
+<div class="feature-box">
+	<p>
+		Vestibulum id ligula porta felis euismod semper. Nulla vitae elit libero, a pharetra augue. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+	</p>
+</div>
 
-//do_action( 'bp_after_member_activity_post_form' );
+<?php
+
+if ( is_user_logged_in() && bp_is_my_profile() && ( !bp_current_action() || bp_is_current_action( 'just-me' ) ) )
+get_template_part( 'activity/post', 'form');
+
+do_action( 'bp_after_member_activity_post_form' );
 do_action( 'bp_before_member_activity_content' ); ?>
 
 <div class="activity" role="main">
