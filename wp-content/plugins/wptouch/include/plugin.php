5131813478a087288563f79211fae7d331f31283
Boon Gorges <bgorges@monti.gmu.edu>
2013-01-08 08:51:59 -0500

    File cleanup

--- wp-content/plugins/wptouch/include/plugin.php
+++
@@ -1,7 +0,0 @@
-<?php
-	function bnc_get_wp_version() {
-		$version = (float)str_replace('.','',get_bloginfo('version'));
-		if ($version < 100) { $version = $version * 10; }
-		$version = $version / 100;
-		return $version; 
-		}
