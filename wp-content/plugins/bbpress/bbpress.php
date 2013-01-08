6ce88d10a01fef0de63d83f9e05a6514fc9ab0f9
Boon Gorges <bgorges@monti.gmu.edu>
2013-01-08 08:45:34 -0500

    Updates plugin: bbPress

--- wp-content/plugins/bbpress/bbpress.php
+++ wp-content/plugins/bbpress/bbpress.php
@@ -5,7 +5,7 @@
  *
  * bbPress is forum software with a twist from the creators of WordPress.
  *
- * $Id: bbpress.php 4439 2012-11-19 06:15:17Z johnjamesjacoby $
+ * $Id: bbpress.php 4567 2012-12-11 09:20:41Z johnjamesjacoby $
  *
  * @package bbPress
  * @subpackage Main
@@ -17,7 +17,7 @@
  * Description: bbPress is forum software with a twist from the creators of WordPress.
  * Author:      The bbPress Community
  * Author URI:  http://bbpress.org
- * Version:     2.2
+ * Version:     2.2.3
  * Text Domain: bbpress
  * Domain Path: /languages/
  */
@@ -180,8 +180,8 @@ final class bbPress {
 
 		/** Versions **********************************************************/
 
-		$this->version    = '2.2';
-		$this->db_version = '220';
+		$this->version    = '2.2.3';
+		$this->db_version = '223';
 
 		/** Paths *************************************************************/
 
@@ -946,4 +946,3 @@ if ( defined( 'BBPRESS_LATE_LOAD' ) ) {
 }
 
 endif; // class_exists check
-
