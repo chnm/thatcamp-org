1246e55bea5663d428142a66ac8ab9d9d52e4b5a
Boone B Gorges <boonebgorges@gmail.com>
2013-04-29 14:26:19 -0400

    Updates THATCamp Karma theme to match production site

---
+++ wp-content/themes/thatcamp-karma/members/single/member-header.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * BuddyPress - Users Header
+ *
+ * @package thatcamp
+ * @since thatcamp 1.0
+ */
+
+?>
+
+<?php do_action( 'bp_before_member_header' ); ?>
+
+<div id="item-header-avatar">
+	<a href="<?php bp_displayed_user_link(); ?>">
+		<?php bp_displayed_user_avatar( 'type=full&width=140&height=140' ); ?>
+	</a>
+	<h4>
+		<a href="<?php bp_displayed_user_link(); ?>"><?php bp_displayed_user_fullname(); ?></a>
+	</h4>
+
+</div>
+<?php do_action( 'bp_after_member_header' ); ?>
+
+<?php do_action( 'template_notices' ); ?>
