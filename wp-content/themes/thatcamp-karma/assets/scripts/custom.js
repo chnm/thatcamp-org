47da3d63985dd6305d7ea2870d6180b6e9494d76
Boone B Gorges <boonebgorges@gmail.com>
2013-05-16 01:09:38 -0400

    Adds js hack to make placeholder text disappear on focus
    See #49

--- wp-content/themes/thatcamp-karma/assets/scripts/custom.js
+++ wp-content/themes/thatcamp-karma/assets/scripts/custom.js
@@ -1,4 +1,11 @@
 jQuery(document).ready(function($){
+	// Make placeholder work the way it should
+	$('input:text, textarea').each(function(){
+		var $this = $(this);
+		$this.data('placeholder', $this.attr('placeholder'))
+		 .focus(function(){$this.removeAttr('placeholder');})
+		 .blur(function(){$this.attr('placeholder', $this.data('placeholder'));});
+	});
 			
 	$( '#ri-grid' ).gridrotator( {
 		rows		: 2,
