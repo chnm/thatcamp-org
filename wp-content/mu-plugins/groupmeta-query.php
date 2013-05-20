76b88e2e8d0e4dec6ab44151852e17ef22fd7dbf
Boone B Gorges <boonebgorges@gmail.com>
2013-05-20 14:31:06 -0400

    Don't use $wpdb->prepare() incorrectly in groupmeta-query.php

--- wp-content/mu-plugins/groupmeta-query.php
+++ wp-content/mu-plugins/groupmeta-query.php
@@ -59,7 +59,7 @@ class BP_Groups_Meta_Filter {
 	function setup_group_ids() {
 		global $wpdb, $bp;
 
-		$sql = $wpdb->prepare( "SELECT group_id FROM {$bp->groups->table_name_groupmeta} WHERE " );
+		$sql = "SELECT group_id FROM {$bp->groups->table_name_groupmeta} WHERE ";
 
 		$join_clauses  = array();
 		$where_clauses = array();
@@ -87,7 +87,7 @@ class BP_Groups_Meta_Filter {
 		}
 
 		if ( !empty( $where_clauses ) ) {
-			$sql = $wpdb->prepare( $wpdb->prepare( "SELECT gmf1.group_id FROM " . implode( ' ', $join_clauses ) . " WHERE " . implode( ' AND ', $where_clauses ) ) );
+			$sql = $wpdb->prepare( "SELECT gmf1.group_id FROM " . implode( ' ', $join_clauses ) . " WHERE " . implode( ' AND ', $where_clauses ) );
 		} else {
 			$sql = $wpdb->get_results( "SELECT id FROM {$bp->groups->table_name} WHERE 1 = 0" );
 		}
