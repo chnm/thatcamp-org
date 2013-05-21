37deaeae302e0f43590d0e02a7da11485aba614a
Boone B Gorges <boonebgorges@gmail.com>
2013-05-21 15:12:01 -0400

    For country/state/province selector, don't show any default text. See #61, #48

--- wp-content/mu-plugins/thatcamp-setup-admin-page.php
+++ wp-content/mu-plugins/thatcamp-setup-admin-page.php
@@ -258,7 +258,7 @@ function thatcamp_country_picker( $args = array() ) {
   <div id="thatcamp-country">
     <span class="thatcamp-location-label">Country:</span>
     <select name="Country" id="country-selector" autofocus="autofocus" autocorrect="off" autocomplete="off">
-      <option value="" selected="selected">Select Country</option>
+      <option value="" selected="selected"></option>
       <option <?php selected( $r['country'], 'Afghanistan' ) ?> value="Afghanistan" data-alternative-spellings="AF افغانستان">Afghanistan</option>
       <option <?php selected( $r['country'], 'Åland Islands' ) ?> value="Åland Islands" data-alternative-spellings="AX Aaland Aland" data-relevancy-booster="0.5">Åland Islands</option>
       <option <?php selected( $r['country'], 'Albania' ) ?> value="Albania" data-alternative-spellings="AL">Albania</option>
@@ -514,7 +514,7 @@ function thatcamp_country_picker( $args = array() ) {
   <div id="thatcamp-state">
     <span class="thatcamp-location-label">State:</span>
     <select name="State" id="state-selector" autofocus="autofocus" autocorrect="off" autocomplete="off">
-      <option value="" selected="selected">Select State</option>
+      <option value="" selected="selected"></option>
       <option <?php selected( $r['state'], 'Alabama' ) ?> value="Alabama" data-alternative-spellings="AL">Alabama</option>
       <option <?php selected( $r['state'], 'Alaska' ) ?> value="Alaska" data-alternative-spellings="AK">Alaska</option>
       <option <?php selected( $r['state'], 'Arizona' ) ?> value="Arizona" data-alternative-spellings="AZ">Arizona</option>
@@ -573,7 +573,7 @@ function thatcamp_country_picker( $args = array() ) {
   <div id="thatcamp-province">
     <span class="thatcamp-location-label">Province:</span>
     <select name="Province" id="province-selector" autofocus="autofocus" autocorrect="off" autocomplete="off">
-      <option value="" selected="selected">Select Province</option>
+      <option value="" selected="selected"></option>
       <option <?php selected( $r['province'], 'Alberta' ) ?> value="Alberta" data-alternative-spellings="AB">Alberta</option>
       <option <?php selected( $r['province'], 'British Columbia' ) ?> value="British Columbia" data-alternative-spellings="BC">British Columbia</option>
       <option <?php selected( $r['province'], 'Manitoba' ) ?> value="Manitoba" data-alternative-spellings="MB">Manitoba</option>
