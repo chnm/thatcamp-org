fab7dc41951a8887dd5353fdb12549da416cee67
Boone B Gorges <boonebgorges@gmail.com>
2013-05-24 01:21:18 -0400

    Changes RSS link and text on home page to point to New Camps feed. See #74

--- wp-content/themes/thatcamp-karma/sidebar-home.php
+++ wp-content/themes/thatcamp-karma/sidebar-home.php
@@ -5,25 +5,25 @@
  * @package thatcamp
  * @since thatcamp 1.0
  */
-?>	
+?>
 <?php do_action( 'bp_before_sidebar' ); ?>
-<div id="sidebar" role="complementary">	
+<div id="sidebar" role="complementary">
 	<?php do_action( 'bp_inside_before_sidebar' ); ?>
 	<!-- demo sidebar content -->
-	
+
 	<a href="<?php echo site_url(); ?>/registry" class="button campbutton">
 		<span class="button-inner"><?php _e( 'Organize a THATCamp', 'thatcamp' ); ?></span>
 	</a>
-		
+
 	<a href="<?php echo site_url(); ?>/contact" class="email-thatcamplink"><?php _e( 'Contact Us', 'thatcamp' ); ?></a>
-	
-	<a href="<?php echo site_url(); ?>/feed" class="rss-thatcamplink"><?php _e( 'RSS', 'thatcamp' ); ?></a>
-	
+
+	<a href="<?php echo site_url(); ?>/camps/feed" class="rss-thatcamplink"><?php _e( 'New THATCamps by RSS', 'thatcamp' ); ?></a>
+
 
 	<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
 		<?php dynamic_sidebar( 'sidebar' ); ?>
 	<?php endif; ?>
-	
+
 	<div id="twitterwidget-wrapper">
 		<?php if ( is_active_sidebar( 'sidebar-twitter' ) ) : ?>
 			<?php dynamic_sidebar( 'sidebar-twitter' ); ?>
@@ -32,8 +32,8 @@
 			<span class="button-inner"><?php _e( 'Follow Us', 'thatcamp' ); ?></span>
 		</a>
 	</div>
-	
-	
+
+
 	<?php do_action( 'bp_inside_after_sidebar' ); ?>
 </div>
 <?php do_action( 'bp_after_sidebar' ); ?>
