e66ce856d1ffd4cd23b2ec82c15a47ad60084892
Amanda French <amanda@amandafrench.net>
2013-06-29 12:53:08 -0400

    Revert "Changes search placeholder text"
    This reverts commit 2fe80cf4fdc63ebf23e177a3fa87297e76676aa8.

--- wp-content/themes/thatcamp-karma/header.php
+++ wp-content/themes/thatcamp-karma/header.php
@@ -58,7 +58,7 @@
 		
 			<div id="search-bar">
 					<form action="<?php echo bp_search_form_action(); ?>" method="post" id="search-form">
-						<input type="text" id="search-terms" name="search-terms" placeholder="Search web pages and blog posts" value="<?php echo isset( $_REQUEST['s'] ) ? esc_attr( $_REQUEST['s'] ) : ''; ?>" />
+						<input type="text" id="search-terms" name="search-terms" placeholder="Looking for..." value="<?php echo isset( $_REQUEST['s'] ) ? esc_attr( $_REQUEST['s'] ) : ''; ?>" />
 
 						<input type="submit" name="search-submit" id="search-submit" value="<?php _e( 'Search', 'thatcamp' ); ?>" />
 						<?php wp_nonce_field( 'bp_search_form' ); ?>
