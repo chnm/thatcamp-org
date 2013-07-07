d7261e6eba0fdfb561c002523ddcd8de79df03d0
Amanda French <amanda@amandafrench.net>
2013-07-07 11:41:29 -0400

    Adds plugin Custom Meta Widget

---
+++ wp-content/plugins/custom-meta-widget/customMeta.php
@@ -0,0 +1,228 @@
+<?php /*
+Plugin Name: Custom Meta Widget
+Plugin URI: http://shinraholdings.com/plugins/custom-meta-widget
+Description: Clone of the standard Meta widget with options to hide thlogin, admin, entry and comment feed, WordPress.org and /custom links.
+Version: 1.4.5
+Author: bitacre
+Author URI: http://shinraholdings.com/
+License: GPLv2
+	Copyright 2013 Shinra Web Holdings (http://shinraholdings.com)
+*/
+
+/**
+ * CLASS customMetaWidget 
+ */
+class customMetaWidget extends WP_Widget { // start of class
+
+/**
+ * Initialize Classwide Variables
+ */
+var $homepage = 'http://shinraholdings.com/plugins/custom-meta-widget/';
+
+/**
+ * CONSTRUCTOR
+ */
+function customMetaWidget() {	
+	// set widget options
+	$widget_ops = array ( 
+		'classname' => 'customMetaWidget',
+		'description' => __( 'Hide the individual log in/out, admin, feed and WordPress links', 'customMetaWidget' )
+	); 
+	
+	// extend widget
+	$this->WP_Widget( 'customMetaWidget', 'Custom Meta', $widget_ops );
+}
+
+
+/**
+ *  Declare Form Input Options 
+ * (not part of WP_Widget class)
+ */
+function get_options() {
+	$keys = array( 'slug', 'type', 'default', 'label', 'before' );
+	
+	$values = array( 
+		'title' => array( 'title', 'text', __( 'Meta', 'customMetaWidget' ), __( 'Title', 'customMetaWidget' ), '' ),			
+		'register' => array( 'register', 'checkbox', 1, __( 'Show "Register/Admin" link?', 'customMetaWidget' ), '' ),
+		'login' => array( 'login', 'checkbox', 1, __( 'Show "Log in/out" link?', 'customMetaWidget' ), '' ),
+		'entryrss' => array( 'entryrss', 'checkbox', 1, __( 'Show "Entries RSS" link?', 'customMetaWidget' ), '' ),
+		'commentrss' => array( 'commentrss', 'checkbox', 1, __( 'Show "Comments RSS" link?', 'customMetaWidget' ), '' ),
+		'wordpress' => array( 'wordpress', 'checkbox', 1, __( 'Show "WordPress.org" link?', 'customMetaWidget' ), '' ),
+		'showcustom' => array( 'showcustom', 'checkbox', 0, __( 'Show the custom link?', 'customMetaWidget' ), 'before' => '' ),
+		'customurl' => array( 'customurl', 'text', '', __( 'URL', 'customMetaWidget' ), ' style="margin-left:20px;"' ),
+		'customtext' => array( 'customtext', 'text', '', __( 'Text', 'customMetaWidget' ), ' style="margin-left:20px;"' ),
+		'linklove' => array( 'linklove', 'checkbox', 0, '<small>' . __( 'An awesome way to support this free plugin!', 'customMetaWidget' ) . '</small>', '' )
+	);
+	
+	// build into multi-array
+	$options = array();
+	foreach( $values as $slug => $sub_values ) {
+		$temp = array();
+		for( $i=0; $i<5; $i++ )
+			$temp[$keys[$i]] = $sub_values[$i];
+		$options[$slug] = $temp;
+	} 
+	return $options;
+}
+
+
+/**
+ * Declare Form Input Defaults
+ * (not part of WP_Widget Class)
+ */
+function get_defaults() {
+	// create container and loop
+	$defaults = array(); 
+	foreach( $this->get_options() as $key => $value )
+		$defaults[$key] = $value['default'];
+	return $defaults;
+}
+
+
+/**
+ * Declare Form Input Keys
+ * (not part of WP_Widget Class)
+ */
+function get_keys() {
+	// create container and loop
+	$keys = array(); 
+	foreach( $this->get_options() as $key => $value )
+			$keys[] = $key;
+	return $keys;
+}
+
+
+/**
+ * Draw Widget Options
+ */
+function form( $instance ) { 
+	// parse instance values over defaults
+	$instance = wp_parse_args( ( array ) $instance, $this->get_defaults() ); 
+
+	// loop through input option
+	foreach( $this->get_options() as $slug => $value ) :
+		extract( $value );
+		$id = $this->get_field_id( $slug );
+		$name = $this->get_field_name( $slug );
+		if( $type == 'text' ) {
+			$value = $instance[$slug];
+			$checked = '';
+			$label = $label . ': ';
+		} else {
+			$checked = checked( $instance[$slug], 1, false );
+			$value = 1;
+		}
+		$label_tag = '<label style="margin:0 3px;" for="' . $id . '">' . $label . '</label>'; 
+		?>
+
+        
+	<!-- <?php echo $slug; ?> -->
+    
+	<p<?php echo $before; ?>><?php echo ( $type == 'text' ? $label_tag : '' ); ?><input class="<?php echo ( $type == 'text' ? 'widefat' : 'check' ); ?>" id="<?php echo $id; ?>" name="<?php echo $name; ?>" type="<?php echo $type; ?>" value="<?php echo $value; ?>" <?php echo $checked; ?>/><?php echo ( $type == 'checkbox' ? $label_tag : '' ); ?></p>
+    
+	<?php endforeach; ?>
+
+	
+
+	<?php // check for errors
+	if( $instance['showcustom'] ) { // IF 'showcustom' is checked, AND
+			
+		if( empty( $instance['customtext']) && empty( $instance['customurl'] ) ) // 1. no link and no URL
+			$message = 'You have a custom link with no URL or text!';
+		
+		elseif( empty( $instance['customtext'] ) ) // 2. no link
+			$message = 'You have a custom link with no text!';
+		
+		elseif( empty( $instance['customurl' ] ) ) // 3. no url
+			$message = 'You have a custom link with no URL!';
+		
+	}
+	if( isset( $message ) ) // set message (or don't)
+		echo '<p style="color:#f00; font-weight:bold;" >' . __( $message, 'customMetaWidget' ) . '</p>';
+}
+
+
+/**
+ * SAVE WIDGET OPTIONS 
+ */
+function update( $new_instance, $old_instance) {
+	$instance = $old_instance; // move over unchanged
+	
+	foreach( $this->get_keys() as $key ) // parse new values over
+		$instance[$key] = $new_instance[$key];
+
+	return $instance;
+}
+
+// ACTUAL WIDGET OUTPUT
+function widget( $args, $instance ) { 
+   	extract( $args, EXTR_SKIP ); // extract arguments
+	$title = empty( $instance['title'] ) ? __( 'Meta', 'customMetaWidget' ) : apply_filters( 'widget_title', $instance['title'] ); // if no title, use default ?>
+    
+<!-- 
+	<?php _e( 'Plugin: Custom Meta Widget', 'customMetaWidget' ); ?>
+	<?php _e( 'Plugin URL', 'customMetaWidget' ); ?>: <?php echo $this->homepage; ?>
+-->
+
+
+	<?php echo $before_widget; // (from theme) ?>
+	<?php echo $before_title . esc_attr( $instance['title'] ) . $after_title; ?>
+	<ul>
+	
+	
+    <?php // ADD LINKS
+	$content = array(
+		'register' => wp_register( '<li>', '</li>', false ),
+		
+		'login' => '<li>' . wp_loginout( NULL, false ) . '</li>',
+		
+		'entryrss' => sprintf( __( '%1$sSyndicate this site using RSS 2.0%2$sEntries %3$sRSS%4$s', 'customMetaWidget' ), 
+			'<li><a href="' . get_bloginfo( 'rss2_url' ) . '" title="', '">', 
+			'<abbr title="' . __( 'Really Simple Syndication', 'customMetaWidget' ) . '">', '</abbr></a></li>' ),
+			
+		'commentrss' => sprintf( __( '%1$sSyndicate this site using RSS 2.0%2$sComments %3$sRSS%4$s', 'customMetaWidget' ),
+			'<li><a href="' . get_bloginfo( 'comments_rss2_url' ) . '" title="', '">',
+			'<abbr title="' . __( 'Really Simple Syndication', 'customMetaWidget' ) . '">', '</abbr></a></li>' ),
+			
+		'wordpress' => '<li><a href="http://wordpress.org/" title="' . 
+			__( 'Powered by WordPress, state-of-the-art semantic personal publishing platform.', 'customMetaWidget' ) . 
+			'">WordPress.org</a></li>', 
+	
+		'showcustom' => ( !empty( $instance['customtext'] ) && !empty( $instance['customurl'] ) ? 
+			'<li><a href="' . esc_url( $instance['customurl'] ) . '">' . esc_attr( $instance['customtext'] ) . '</a></li>' :
+		'<!--' . __( 'Error: "Show Custom Link" is checked, but either the text or URL for that link are not specified. The link was not displayed because it would be broken. Check the settings for your Custom Meta widget.', 'customMetaWidget' ) . '-->' ),
+
+		'linklove' => '<li><a href="' . $this->homepage . '" title="' . __( 'WordPress Plugin Homepage', 'customMetaWidget' ) . '">' . __( 'Custom Meta', 'customMetaWidget' ) . '</a></li>' 
+	
+	);
+	
+
+	foreach( $content as $checked => $output )
+		if( (int) esc_attr( $instance[$checked] ) === 1 ) echo $output; ?>
+	
+	</ul>
+    
+	<?php echo $after_widget;
+
+}
+
+} // end class 
+
+
+/**
+ * Unregister WP_Widget_Meta
+ */
+function customMetaWidget_swap() {
+	unregister_widget( 'WP_Widget_Meta' );
+	register_widget( 'customMetaWidget' );
+} add_action( 'widgets_init', 'customMetaWidget_swap' ); // hook
+
+
+/**
+ * Load TextDomain
+ */
+function customMetaWidget_i18n() {
+	load_plugin_textdomain( 'customMetaWidget', NULL, trailingslashit( basename( dirname(__FILE__) ) ) . 'lang' );
+} add_action( 'plugins_loaded', 'customMetaWidget_i18n' ); // hook
+
+?>
---
+++ wp-content/plugins/custom-meta-widget/readme.txt
@@ -0,0 +1,186 @@
+=== Custom Meta Widget ===
+Plugin URI: http://shinraholdings.com/plugins/custom-meta-widget/
+Contributors: bitacre
+Donate link: http://shinraholdings.com/donate
+Tags: meta,hide,link,admin,simple,widget,default,wordpress.org,change,rss,remove,login
+Requires at least: 2.8
+Tested up to: 3.5.1
+Stable tag: 1.4.5
+
+Clone of the standard Meta widget plus options to hide log in/out, admin, feed and WordPress.org/custom links.
+
+== Description ==
+This plugin acts in exactly the same way as the standard Meta widget except that it adds options which allow you control which of the 5 standard Meta widget links should be displayed, with the additional option to add your own custom link.
+
+The standard Meta widget that comes with WordPress offers no customization and requires you to show all 5 of the standard links (register, login, entries RSS, comments RSS, and WordPress.org).
+
+== Installation ==
+1. Download the latest zip file and extract the `custom-meta-widget` directory.
+2. Upload this directory inside your `/wp-content/plugins/` directory.
+3. Activate the 'Custom Meta Widget' on the 'Plugins' menu in WordPress.
+4. Add the 'Custom Meta' widget to your sidebar (using the Appearance > Widgets menu) and choose which customizations you would like.
+
+== Frequently Asked Questions ==
+= Why does my Meta widget look the same? =
+This plugin does not modify the standard WordPress Meta widget. It is a completely separate widget titled 'Custom Meta.' If you are currently using the standard Meta widget, you will have to remove it from your sidebar and replace it with the Custom Meta widget using the Appearance > Widgets menu.
+
+
+It also looks identical to the standard Meta Widget unless you opt to change which links are shown/hidden in the Appearance > Widgets menu, under the expanded 'Custom Meta' widget.
+
+= Which links can I hide/show? =
+You can remove any, all, or none of the links which appear on the standard Meta widget. You are also able to add a custom link to the list if you wish.
+
+= Why did you make this? =
+I didn't like having the 'Comments RSS' link on the sidebar, but did like having the rest of them. I couldn't find a plugin to do that and manually editing the code to accomplish this, while simple, is a dirty hack that breaks with every WordPress update, so I created a crude plugin to remove just the 'Comments RSS' link. I then cleaned this up and added an options page for all the links so that no one else would have to do this in the future.
+
+= Can you add this feature I just thought of? =
+Can I? Yes. Will I? Yes, if I think it would be a helpful addition. I'm trying to keep things clean and simple, but there's always room for improvement, so let me know if you think a feature is lacking!
+
+= Will vanilla WordPress ever implement some variation of this? =
+I don't know man. They haven't yet, so it doesn't look very likely. But I think they should and I would be glad if they did.
+
+= Why did you add an option to link to the plugin site, you horrible sellout? =
+Most people, myself included, are understandably reluctant to donate real currency for free stuff. However, putting a link to my site from your site is just as helpful and a much more comfortable and forthcoming mode of charity, especially in the webmaster community. Also, because I'm a horrible sellout.
+
+== Screenshots ==
+1. The widget's options page on the Appearance > Widgets menu.
+2. The widget displayed on the sidebar with 3 links hidden and a custom link added.
+3. Comparison of options between the standard and Custom Meta Widget.
+
+== Changelog ==
+= 1.4.5 =
+* Wordpress 3.5.1 update
+* Added sk_SK (Slovak) translation
+
+= 1.4.4 =
+* Added de_DE (German) translation
+* General update to language files
+* Major code efficiency upgrades
+
+= 1.4.3 =
+* Small correction to the ru_RU (Russian) translation
+
+= 1.4.2 =
+* Wordpress 3.5 update
+* Added translations for ru_RU
+
+= 1.4.1 =
+* Added missed textdomains for more complete i18n
+* Added translations for da_DK, en_NZ, en_PH, and en_NZ
+* Updated/fixed all translation files
+
+= 1.4.0 =
+* Code efficiency improvements (better loops)
+* Now removes default Meta Widget (to avoid confusion)
+
+= 1.3.6 =
+* Small metadata update
+
+= 1.3.5_a =
+* Updated for WordPress version 3.3.2
+* Fixed the 1 missed textdomain for i18n
+
+= 1.3.5 =
+* Fixed typo introduced in i18n transition.
+* Adds translation files for en-AU, en-CA, en-GB, en-IE, and es-MX.
+
+= 1.3.4 =
+* Fixed minor issues with i18n text domains.
+
+= 1.3.3 =
+* Internationalized all displayed text.
+
+= 1.3.2 =
+* Added additional error handling for the custom link url input.
+* Added an option to beg you for link love (disabled by default, and not annoying).
+* Slightly changed widget options to look more like the vanilla widget's.
+
+= 1.3.1 =
+* Added error handling to prevent blank custom links from being inserted on the sidebar.
+* Cleaned up code formatting.
+
+= 1.3 =
+* Added a custom link option (disabled by default).
+* Updated screenshots 1-3.
+* Changed plugin name on Appearance > Widgets menu to 'Custom Meta' instead of 'Custom Meta Widget' which was redundant.
+* Added additional code commenting.
+
+= 1.2 =
+* Fixed reference to non-existent instance error.
+
+= 1.1 =
+* Minor improvements to code architecture.
+* Increased compliance with WordPress style standards.
+* Significantly more code commenting.
+
+= 1.0 =
+* First released version. 
+* There may still be bugs, but I can't find any. 
+
+== Upgrade Notice ==
+= 1.4.5 =
+Optional upgrade, new translation sk_SK (Slovak).
+
+= 1.4.4 =
+Recommended upgrade, new languages and performance improvement.
+
+= 1.4.3 =
+Small correction to ru_RU (Russian) language translation files, recommended upgrade.
+
+= 1.4.2 =
+Russian language files and WordPress 3.5 compatability update, not a critical upgrade.
+
+= 1.4.1 =
+Fixes some i18n issues and adds additional languages, recommended upgrade.
+
+= 1.4.0 =
+Improves code efficiency, new features, recommended upgrade.
+
+= 1.3.6 =
+Updated metadata, not an essential upgrade.
+
+= 1.3.5_a =
+Fixes one textdomain missed in the internationalization update and update for Wordpress 3.3.2.
+
+= 1.3.5 =
+Adds translations for several languages.
+
+= 1.3.4 =
+Minor improvements to i18n for translators.
+
+= 1.3.3 =
+Adds internationalization, helpful for non-english users.
+
+= 1.3.2 =
+Adds some more error handling, small cosmetic tweaks, and shameless self-promotion.
+
+= 1.3.1 =
+Adds error handling if a custom link with no url/text is added.
+
+= 1.3 =
+Adds new custom link functionality.
+
+= 1.2 =
+Fixes a code error.
+
+= 1.1 =
+Not essential, no bug fixes just minor structural code changes. (Mostly a test of the SVN repository system which I am new at.)
+
+== Translation Help ==
+If you would like to help make this plugin available in your native language, please send your `.po` file to [plugins@shinraholdings.com](mailto:plugins@shinraholdings.com), and I will include it in the next version. The all the language files are in the `lang` subdirectory of the plugin.
+
+* en_XX - [bitacre](mailto:lang@shinraholdings.com)
+* da_DK - [Kris Thomsen](mailto:mail@kristhomsen.dk)
+* ru_RU - VadDevArt
+* de_DE - [Dr. Paolo Colombani](mailto:paolo.colombani@gmail.com)
+* sk_SK - [Branco Radenovich](http://webhostinggeeks.com/user-reviews/)
+
+== Readme Generator ==
+* This plugin's readme.txt file was generated by the [bitacre Readme Generator](http://shinraholdings.com/tools/readme-gen/) for WordPress Plugins.
+
+== Support ==
+* [Plugin Homepage](http://shinraholdings.com/plugins/custom-meta-widget/)
+* [plugins@shinraholdings.com](mailto:plugins@shinraholdings.com)
+
+== Donations ==
+[Donations](http://shinraholdings.com/donate) are graciously accepted to support the continued development and maintenance of this and other plugins. We currently accept Paypal, link backs, and kind words. Also, checking the 'show plugin link' option on the widget helps us out greatly!
