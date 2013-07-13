0008439598dd26f35a928828f29844d18f19f7f6
Amanda French <amanda@amandafrench.net>
2013-07-13 08:22:37 -0400

    Adds Transposh widget area to all sidebars and removes unused Stream sidebar

--- wp-content/themes/thatcamp-karma/sidebar-home.php
+++ wp-content/themes/thatcamp-karma/sidebar-home.php
@@ -23,6 +23,12 @@
 		<?php dynamic_sidebar( 'sidebar' ); ?>
 	<?php endif; ?>
 
+	<div id="transposh-wrapper">
+		<?php if ( is_active_sidebar( 'sidebar-transposh' ) ) : ?>
+			<?php dynamic_sidebar( 'sidebar-transposh' ); ?>
+		<?php endif; ?>
+	</div>
+
 	<div id="twitterwidget-wrapper">
 		<?php if ( is_active_sidebar( 'sidebar-twitter' ) ) : ?>
 			<?php dynamic_sidebar( 'sidebar-twitter' ); ?>
--- wp-content/themes/thatcamp-karma/sidebar.php
+++ wp-content/themes/thatcamp-karma/sidebar.php
@@ -5,14 +5,20 @@
  * @package thatcamp
  * @since thatcamp 1.0
  */
-?>	
+?>
 <?php do_action( 'bp_before_sidebar' ); ?>
-<div id="sidebar" role="complementary">	
+<div id="sidebar" role="complementary">
 	<?php do_action( 'bp_inside_before_sidebar' ); ?>
 	<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
 		<?php dynamic_sidebar( 'sidebar' ); ?>
 	<?php endif; ?>
-	
+
+	<div id="transposh-wrapper">
+		<?php if ( is_active_sidebar( 'sidebar-transposh' ) ) : ?>
+			<?php dynamic_sidebar( 'sidebar-transposh' ); ?>
+		<?php endif; ?>
+	</div>
+
 	<div id="twitterwidget-wrapper">
 		<?php if ( is_active_sidebar( 'sidebar-twitter' ) ) : ?>
 			<?php dynamic_sidebar( 'sidebar-twitter' ); ?>
