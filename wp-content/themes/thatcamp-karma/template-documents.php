e821e29a4ecab53713b7bb3056f77dcd17e3a69e
Amanda French <amanda@amandafrench.net>
2013-07-17 11:41:08 -0400

    Add next and previous links to Documents template

--- wp-content/themes/thatcamp-karma/template-documents.php
+++ wp-content/themes/thatcamp-karma/template-documents.php
@@ -15,6 +15,33 @@
 		<?php while ( have_posts() ) : the_post(); ?>
 			<?php get_template_part( 'parts/content', 'page' );?>
 		<?php endwhile; ?>
+		
+		<?php // Add Next and Previous page links
+		$pagelist = get_pages('sort_column=menu_order&sort_order=asc');
+		$pages = array();
+		foreach ($pagelist as $page) {
+		$pages[] += $page->ID;
+		}
+
+		$current = array_search(get_the_ID(), $pages);
+		$prevID = $pages[$current-1];
+		$nextID = $pages[$current+1];
+		?>
+
+		<div id="nav-below">
+		<?php if (!empty($prevID)) { ?>
+		<div class="nav-previous">
+		<a href="<?php echo get_permalink($prevID); ?>"
+		title="<?php echo get_the_title($prevID); ?>">&larr;&nbsp;<?php echo get_the_title($prevID); ?></a>
+		</div>
+		<?php }
+		if (!empty($nextID)) { ?>
+		<div class="nav-next">
+		<a href="<?php echo get_permalink($nextID); ?>" 
+		 title="<?php echo get_the_title($nextID); ?>"><?php echo get_the_title($nextID); ?>&nbsp;&rarr;</a>
+		</div>
+		<?php } ?>
+		</div><!-- .nav-below -->
 		</div>
 	</div>
 </div>
