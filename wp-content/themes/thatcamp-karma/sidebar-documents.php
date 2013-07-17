1bd8b3e96042d8ac7ffa2a941ddb373ada09f5c4
Amanda French <amanda@amandafrench.net>
2013-07-17 11:27:03 -0400

    Add full menu to Documents sidebar

--- wp-content/themes/thatcamp-karma/sidebar-documents.php
+++ wp-content/themes/thatcamp-karma/sidebar-documents.php
@@ -5,35 +5,19 @@
  * @package thatcamp
  * @since thatcamp 1.0
  */
-?>	
-<div id="sidebar" role="complementary">	
-		<nav id="side-nav" role="navigation">
-			<?php
-			  if($post->post_parent) {
-			  $children = wp_list_pages("title_li=&child_of=".$post->post_parent."&echo=0");
-			  $titlenamer = get_the_title($post->post_parent);
-			  }
-
-			  else {
-			  $children = wp_list_pages("title_li=&child_of=".$post->ID."&echo=0");
-			  $titlenamer = get_the_title($post->ID);
-			  }
-			  if ($children) { ?>
-
-			  <h2> <?php echo $titlenamer; ?> </h2>
-			  <ul class="side_menu">
-			  <?php echo $children; ?>
-			  </ul>
-
-			<?php } ?>
-
-			<?php if ( is_active_sidebar( 'sidebar-documents' ) ) : ?>
-			<?php dynamic_sidebar( 'sidebar-documents' ); ?>
-			<?php endif; ?>					
-		</nav>
+?>
+<div id="sidebar" role="complementary">
+	<h2>Help</h2>
+		<?php wp_nav_menu( array(
+				'theme_location' => 'documents',
+				'menu' => 'documents',
+				'menu_class' => 'side_menu',
+				'container' => 'nav',
+				'container_id' => 'side-nav',
+		)); ?>	
 	<div id="transposh-wrapper">
 		<?php if ( is_active_sidebar( 'sidebar-transposh' ) ) : ?>
-			<?php dynamic_sidebar( 'sidebar-transposh' ); ?>
+		<?php dynamic_sidebar( 'sidebar-transposh' ); ?>
 		<?php endif; ?>
 	</div>
 </div>
