4d65f9fc4a471474da9bdadb74764de220721e1b
Amanda French <amanda@amandafrench.net>
2013-11-19 14:03:36 -0500

    Edit version number on Transposh plugin to prevent update

--- wp-content/plugins/transposh-translation-filter-for-wordpress/transposh.php
+++ wp-content/plugins/transposh-translation-filter-for-wordpress/transposh.php
@@ -5,7 +5,7 @@
   Plugin URI: http://transposh.org/
   Description: Translation filter for WordPress, After enabling please set languages at the <a href="admin.php?page=tp_main">the options page</a> Want to help? visit our development site at <a href="http://trac.transposh.org/">trac.transposh.org</a>.
   Author: Team Transposh
-  Version: 0.9.3
+  Version: 0.9.4
   Author URI: http://transposh.org/
   License: GPL (http://www.gnu.org/licenses/gpl.txt)
   Text Domain: transposh
