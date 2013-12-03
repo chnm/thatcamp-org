43c112592b7af25419d312988b9c5f7bfd1943a1
Amanda French <amanda@amandafrench.net>
2013-12-03 12:36:47 -0500

    Update config files

--- wp-content/wp-cache-config.php
+++ wp-content/wp-cache-config.php
@@ -5,6 +5,7 @@ WP-Cache Config Sample File
 See wp-cache.php for author details.
 */
 
+$dismiss_gc_warning = 1; //Added by WP-Cache Manager
 $cache_domain_mapping = '1'; //Added by WP-Cache Manager
 $cache_schedule_interval = 'weekly'; //Added by WP-Cache Manager
 $cache_gc_email_me = 0; //Added by WP-Cache Manager
