b6658d5770ee47e31014df742c2f713f474ac031
Amanda French <amanda@amandafrench.net>
2014-02-14 09:05:41 -0500

    Add plugin compact-archives

---
+++ wp-content/plugins/compact-archives/compact.php
@@ -0,0 +1,307 @@
+<?php
+/*
+Plugin Name: WPBeginner's Compact Archives
+Plugin URI: http://www.wpbeginner.com
+Description: Displays a compact monthly archive instead of the default long list. Either display it as a block suitable for the body of a page or in a form compact enough for a sidebar. 
+Version: 3.0.3
+Author: WPBeginner
+Author URI: http://www.wpbeginner.com
+*/
+
+/* 
+Maintained and supported by WPBeginner, this plugin was originally developed by Rob Marsh. Copyright 2008  Rob Marsh, SJ  (http://rmarsh.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/*
+	Display the monthly archive of posts in a more compact form than the usual long list.
+	
+	If $style == 'block' the display will be wide enough to fill the main column of a page:
+	
+		2006: Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec 
+		2005: Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec 
+		2004: Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec 
+		
+	If $style == 'initial' (the default) the display will fit into a sidebar.
+
+				2006: J F M A M J J A S O N D 
+				2005: J F M A M J J A S O N D 
+				2004: J F M A M J J A S O N D 
+
+	If $style == 'numeric' the display will show month numbers.
+
+				2006: 01 02 03 04 05 06 07 08 09 10 11 12
+				2005: 01 02 03 04 05 06 07 08 09 10 11 12
+				2004: 01 02 03 04 05 06 07 08 09 10 11 12
+
+	$before and $after wrap each line of output. The default values are suitable for such use as:
+
+		<ul>
+			<?php compact_archive(); ?>
+		</ul>
+		
+	Should work fine with whatever kind of permalink you are using.
+	The month abbreviations should adapt to the locale set in wp-config.php.
+	The year link at the start of each line is wrapped in <strong></strong> and months with no posts 
+	are wrapped in <span class="emptymonth"></span> so you can differentiate them visually
+	
+	If my Post Output Plugin is installed the Compact Archive output will be cached for efficiency.
+	
+*/
+function compact_archive( $style='initial', $before='<li>', $after='</li>' ) {
+ 	$result = false;
+	// if the Plugin Output Cache is installed we can cheat...
+	if (defined('POC_CACHE_4')) {
+		$key = 'c_a'.$style.$before.$after;
+		poc_cache_timer_start();
+		$result = poc_cache_fetch($key);
+		if ($result) $cache_time = sprintf('<!-- Compact Archive took %.3f milliseconds from the cache -->', 1000 * poc_cache_timer_start());
+	}
+	// ... otherwise we do it the hard way
+	if (false === $result) {
+		$result = utf8_encode(get_compact_archive($style, $before, $after));
+		if (defined('POC_CACHE_4')) {
+			poc_cache_store($key, $result);
+			$cache_time = sprintf('<!-- Compact Archive took %.3f milliseconds -->', 1000 * poc_cache_timer_start());			
+		}
+	} 
+
+	echo $result;
+
+if (defined('POC_CACHE_4')) echo  $cache_time;
+
+}
+
+/********************************************************************************************************
+	Stuff below this point is not meant to be used directly
+*********************************************************************************************************/
+
+function get_compact_archive( $style='initial', $before='<li>', $after='</li>' ) {
+	global $wpdb, $wp_version;
+	setlocale(LC_ALL,WPLANG); // set localization language
+	$below21 = version_compare($wp_version, '2.1','<');
+	// WP 2.1 changed the way post_status and post_type fields work
+	if ($below21) {
+		$now = current_time('mysql');
+		$results = $wpdb->get_results("SELECT DISTINCT YEAR(post_date) AS year, MONTH(post_date) AS month FROM " . $wpdb->posts . " WHERE post_date <'" . $now . "' AND post_status='publish' AND post_password='' ORDER BY year DESC, month DESC");
+	} else {
+		$results = $wpdb->get_results("SELECT DISTINCT YEAR(post_date) AS year, MONTH(post_date) AS month FROM " . $wpdb->posts . " WHERE post_type='post' AND post_status='publish' AND post_password='' ORDER BY year DESC, month DESC");
+	}
+	if (!$results) {
+		return $before.__('Archive is empty').$after;
+	}
+	$dates = array();
+	foreach ($results as $result) {
+		$dates[$result->year][$result->month] = 1;
+	}
+	unset($results);
+	$result = '';
+	foreach ($dates as $year => $months){
+		$result .= $before.'<strong><a href="'.get_year_link($year).'">'.$year.'</a>: </strong> ';
+		for ( $month = 1; $month <= 12; $month += 1) {
+			$month_has_posts = (isset($months[$month]));
+			$dummydate = strtotime("$month/01/2001");
+			// get the month name; strftime() localizes
+			$month_name = strftime("%B", $dummydate); 
+			switch ($style) {
+			case 'initial':
+				$month_abbrev = $month_name[0]; // the inital of the month
+				break;
+			case 'block':
+				$month_abbrev = strftime("%b", $dummydate); // get the short month name; strftime() localizes
+				break;
+			case 'numeric':
+				$month_abbrev = strftime("%m", $dummydate); // get the month number, e.g., '04'
+				break;
+			default:
+				$month_abbrev = $month_name[0]; // the inital of the month
+			}
+			if ($month_has_posts) {
+				$result .= '<a href="'.get_month_link($year, $month).'" title="'.$month_name.' '.$year.'">'.$month_abbrev.'</a> ';
+			} else {
+				$result .= '<span class="emptymonth">'.$month_abbrev.'</span> ';
+			}
+		}
+		$result .= $after."\n";
+	}
+	return $result;
+}
+
+// Compact Archive Shortcode 
+
+function compact_archives_shortcode($atts) { 
+extract( shortcode_atts( array(
+		'style' => 'initial',
+		'before' => '<li>',
+		'after' => '</li>'
+	), $atts ) );
+	if ($before == "<li>")	:
+		$wrap = "<ul>";
+	endif; 
+
+	if ($after == "</li>") :
+$wrap_end = "</ul>";
+endif;
+
+$string = $wrap . get_compact_archive($style, $before, $after) . $wrap_end;  
+
+return $string;
+
+} 
+
+add_shortcode( 'compact_archive', 'compact_archives_shortcode' );
+
+// Compact Archive Widget
+// Thanks to Aldo Latino http://www.aldolat.it/
+
+function wpb_caw_load_widget() {
+	register_widget( 'WPBeginner_CAW_Widget' );
+}
+add_action( 'widgets_init', 'wpb_caw_load_widget' );
+
+/**
+ * Create the widget
+ *
+ */
+class WPBeginner_CAW_Widget extends WP_Widget {
+	function WPBeginner_CAW_Widget() {
+		$widget_ops = array(
+			'classname'   => 'wpb_caw_widget',
+			'description' => __( 'Create a widget for Compact Archives plugin', 'wpb-caw-domain' )
+		);
+		$this->WP_Widget( 'wpb-caw-widget', __( 'Compact Archives Widget', 'wpb-caw-domain' ), $widget_ops );
+	}
+
+	function widget( $args, $instance ) {
+		extract( $args );
+
+		$title = apply_filters( 'widget_title', $instance['title'] );
+		$widget_style = $instance['style'];
+		switch( $instance['text_style'] ) {
+		    case 'none' :
+		        $text_style = '';
+		        break;
+		    case 'uppercase' :
+		        $text_style = ' style="text-transform: uppercase;"';
+		        break;
+		    case 'capitalize':
+		        $text_style = ' style="text-transform: capitalize;"';
+		        break;
+		}
+
+		echo $before_widget;
+		if ( $title ) echo $before_title . $title . $after_title; ?>
+		<ul class="compact-archives"<?php echo $text_style; ?>>
+			
+				<?php compact_archive( $style = $widget_style ); ?>
+			
+				
+		</ul>
+		<?php echo $after_widget;
+	}
+
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['style'] = $new_instance['style'];
+		$instance['text_style'] = $new_instance['text_style'];
+		return $instance;
+	}
+
+	function form( $instance ) {
+		$defaults = array(
+			'title'      => __( 'Archives by Month', 'caw-domain' ),
+			'style'      => 'initial',
+			'text_style' => 'uppercase'
+		);
+		$instance = wp_parse_args( (array) $instance, $defaults );
+		$style = $instance['style'];
+		$text_style = $instance['text_style'];
+		
+/**
+ * Check if Compact Archives Widget plugin is installed. 
+ * Ask user to deactivate it if it is installed. 
+ * 
+ */
+	
+	if ( is_plugin_active( 'compact-archives-widget/compact-archives-widget.php' ) ) { ?>
+			<p style="background-color: #FFD5D5; padding: 10px;">
+				<?php printf( __( '%3$sNotice.%4$s<br />The %3$sCompact Archives Widget%4$s plugin is active on your WordPress and it\'s not needed anymore . Please, %1$sdeactivate it from here%2$s.', 'wpb-caw-domain' ),
+					'<a href="' . admin_url( 'plugins.php' ) . '">',
+					'</a>',
+					'<strong>',
+					'</strong>'
+				); ?>
+			</p>
+		<?php } ?>
+			<p>
+				<label for="<?php echo esc_attr( $this->get_field_id('title') ); ?>">
+					<?php _e( 'Title:', 'wpb-caw-domain' ); ?>
+				</label>
+				<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $instance['title'] ); ?>" />
+			</p>
+
+			<p>
+				<label for="<?php echo $this->get_field_id( 'style' ); ?>">
+					<?php _e( 'Select the style:', 'wpb-caw-domain' ); ?>
+				</label><br />
+				<select name="<?php echo $this->get_field_name( 'style' ); ?>" >
+					<option <?php selected( 'initial', $style ); ?> value="initial">
+						<?php _e( 'Initials', 'wpb-caw-domain' ); ?>
+					</option>
+					<option <?php selected( 'block', $style ); ?> value="block">
+						<?php _e( 'Block', 'wpb-caw-domain' ); ?>
+					</option>
+					<option <?php selected( 'numeric', $style ); ?> value="numeric">
+						<?php _e( 'Numeric', 'wpb-caw-domain' ); ?>
+					</option>
+				</select>
+			</p>
+
+			<p>
+				<label for="<?php echo $this->get_field_id( 'text_style' ); ?>">
+					<?php _e( 'Transform text:', 'wpb-caw-domain' ); ?>
+				</label>
+				<select name="<?php echo $this->get_field_name( 'text_style' ); ?>" >
+					<option <?php selected( 'None', $text_style ); ?> value="none">
+						<?php _e( 'None transformation', 'wpb-caw-domain' ); ?>
+					</option>
+					<option <?php selected( 'uppercase', $text_style ); ?> value="uppercase">
+						<?php _e( 'UPPERCASE', 'wpb-caw-domain' ); ?>
+					</option>
+					<option <?php selected( 'capitalize', $text_style ); ?> value="capitalize">
+						<?php _e( 'Capitalize', 'wpb-caw-domain' ); ?>
+					</option>
+				</select>
+			</p>
+		<?php
+	}
+}
+
+/**
+ * Make plugin available for i18n
+ * Translations must be archived in the /languages directory
+ *
+ * 
+ */
+
+function wpb_caw_load_languages() {
+	load_plugin_textdomain( 'wpb-caw-domain', false, dirname( plugin_basename( __FILE__ ) ) . '/languages');
+}
+
+add_action( 'plugins_loaded', 'wpb_caw_load_languages' );
+
+?>
---
+++ wp-content/plugins/compact-archives/readme.txt
@@ -0,0 +1,147 @@
+=== Compact Archives ===
+Contributors: smub, noumaan
+Tags: posts, archive, archives
+Requires at least: 3.0
+Tested up to: 3.5.1
+Stable tag: 3.0.3
+
+Displays a monthly archive of posts in a more compact form than the usual long list.
+
+== Description ==
+
+The built-in WordPress archives widget is great for new blogs, but it doesn't look as good for more established blogs like [WPBeginner](http://www.wpbeginner.com "WPBeginner"). Compact Archives displays the monthly archive of posts in a more compact form than the usual long list. It can be shown as a compact block suitable for the body of an archives page:
+
+`2009: Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec 
+2008: Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec 
+2007: Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec`
+
+or in an even more compact form to fit a sidebar:
+
+`2009: J F M A M J J A S O N D 
+2008: J F M A M J J A S O N D 
+2007: J F M A M J J A S O N D` 
+				
+or something in between:
+
+`2009: 01 02 03 04 05 06 07 08 09 10 11 12
+2008: 01 02 03 04 05 06 07 08 09 10 11 12
+2007: 01 02 03 04 05 06 07 08 09 10 11 12`
+
+If you like this plugin, and want to learn more about WordPress, then I suggest that you visit [WPBeginner](http://www.wpbeginner.com "WPBeginner"). It is one of the largest free WordPress resource site with tons of [WordPress tutorials](http://www.wpbeginner.com/category/wp-tutorials/ "WordPress tutorials"), and I'm proud to say that I created it. 
+
+We also have [free WordPress video tutorials](http://videos.wpbeginner.com "Checkout out WPBeginner's WordPress tutorial videos"), and numerous other guides such as [how to choose the best WordPress hosting](http://www.wpbeginner.com/wordpress-hosting/ "How to Choose the Best WordPress Web Hosting"), [how to install WordPress](http://www.wpbeginner.com/how-to-install-wordpress/ "How to install WordPress") and many more.
+
+I often get asked what are the [best WordPress plugins](http://www.wpbeginner.com/category/plugins/ "best WordPress plugins")? Which plugins should I use on my site? What are the must have plugins? I have created a blueprint of all the plugins and tools that I'm using. [Take a look at plugins I'm using at WPBeginner](http://www.wpbeginner.com/blueprint/ "Take a look at plugins I'm using at WPBeginner")
+
+Note for Beginners: Like all plugins, Compact Archives is only available for self-hosted WordPress sites. So YES you need to [switch from WordPress.com to WordPress.org](http://www.wpbeginner.com/wp-tutorials/how-to-properly-move-your-blog-from-wordpress-com-to-wordpress-org/ "switch from WordPress.com to WordPress.org") in order to use this plugin on your WordPress site. For more details, see my infographic on [Self hosted WordPress.org vs Free WordPress.com](http://www.wpbeginner.com/beginners-guide/self-hosted-wordpress-org-vs-free-wordpress-com-infograph/ "Self Hosted WordPress.org vs free WordPress.com")
+
+If you like this plugin, then please leave a good rating. For support just ask the questions here in the support forum.
+
+Lastly, if you like this plugin then follow WPBeginner on [Twitter](http://twitter.com/wpbeginner "Twitter"), [Facebook](http://facebook.com/wpbeginner "Facebook"), and [Google+](https://plus.google.com/101634180904808003404/ "Google+")
+
+
+Plugin Header Photo Credit: [Mr. T in DC](http://www.flickr.com/photos/mr_t_in_dc/4249887012/ "Scroll photo by Mr T in DC's Flickr")
+
+Original Author Credit: This plugin was originally created by [Rob Marsh](http://profiles.wordpress.org/robmarsh/) and [Aldolat](http://profiles.wordpress.org/aldolat/) added a widget for it. WPBeginner adopted the plugin and took the responsibility to keep it updated and going.
+
+
+== Installation ==
+
+1. Upload the plugin folder to your /wp-content/plugins/ folder.
+
+1. Go to the **Plugins** page and activate the plugin.
+
+1. Put `<?php compact_archive(); ?>` at the place in your template 
+where you want it to appear, e.g., in your sidebar:
+
+`	<ul>
+		<?php compact_archive(); ?>
+	</ul>`
+
+1. You can also use shortcode `[compact_archive]` in a page or post to display compact archives. See FAQ for shortcode parameters. 
+
+1. There is also a Compact Archives Widget which can be used to display compact archives in sidebar or any widgetized area. 
+
+== Frequently Asked Questions ==
+
+= How Do I Get the Different Layouts? =
+
+The template tag, `compact_archive`, has some parameters:
+
+`	compact_archive($style='initial', $before='<li>', $after='</li>');`
+	
+If $style == 'initial' (the default) the display will fit into a sidebar:
+
+`	2009: J F M A M J J A S O N D 
+	2008: J F M A M J J A S O N D 
+	2007: J F M A M J J A S O N D` 
+
+If $style == 'block' the display will be wide enough to fill the main column of a page:
+
+`	2009: Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec 
+	2008: Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec 
+	2007: Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec`
+
+If $style == 'numeric' the display will use month numbers:
+
+`	2009: 01 02 03 04 05 06 07 08 09 10 11 12
+	2008: 01 02 03 04 05 06 07 08 09 10 11 12
+	2007: 01 02 03 04 05 06 07 08 09 10 11 12`
+
+$before and $after wrap each line of output. The default values make 
+each line of the archive into a list item:
+
+`	<ul>
+		<?php compact_archive(); ?>
+	</ul>`
+
+= How do I get different layouts using shortcode? =
+
+The shortcode `[compact_archive]` works just like the template tag. It accepts three parameters which are style, before, and after. 
+
+Using shortcode `[compact_archive style="block"]` will display compact archives in block. 
+
+Using shortcode `[compact_archive style="numeric" before="<p>" after="</p>"]` will display compact archive in numeric form, wrapped in a paragraph tag.  
+
+= How do I get different layouts using Compact Archive Widget in Sidebar? =
+
+Compact Archive comes with a sidebar widget located Appearance -> Widgets. Drag and drop Compact Archives Widget to your sidebar. The widget has three option fields. You can provide your widget a title if you want or leave it blank. You can choose a display layout from Select the Style option. It has three layout choices to choose from which are initials, block, and numeric. Tansform text option can be left to No Transformation or you can tansform text style to UPPERCASE or Capitalize.  
+
+= Is There Any Scope for CSS Styling? =
+
+The year links at the start of each line are wrapped in <strong></strong> tags while months with no posts are wrapped with <span class="emptymonth"></span> so you can differentiate them visually using your style sheet.
+
+= What if My Site is in Another Language? =
+
+The plugin chooses month names and abbreviations according to the language locale, WPLANG, set in wp-config.php.
+
+Compact Archives also honours whatever kind of permalink pattern you have set.
+
+== Screenshots ==
+
+1. Compact archives displayed in block layout
+
+== Changelog ==
+= 3.0.3 =
+* Minor bug fix
+= 3.0.2 =
+* Resolved shortcode output bug.
+= 3.0.1 =
+* Resolved function naming conflict.
+* Added a check to see if compct archives widget plugin is installed. If it is installed then ask the user to deactivate it. 
+= 3.0.0 =
+* Added shortcode to be used in posts and pages
+* Added widget based on Compact Archives Widget plugin by [aldolat](http://profiles.wordpress.org/aldolat/)
+= 2.0.0 =
+* 3-10 x faster depending on number of posts
+* makes the most of [Plugin Output Cache v.4.x](http://rmarsh.com/plugins/poc-cache/)
+= 1.0.6 = 
+* Introduces numeric display
+= 1.0.5 = 
+* Fixes a display bug for locales with accented characters
+= 1.0.4 = 
+* Makes use of the Plugin Output Cache to speed things up
+= 1.0.3 =
+* Fixes a small problem with undated posts 
+= 1.0.1 = 
+* Speeds up the plugin for WordPress 2.1+ 
