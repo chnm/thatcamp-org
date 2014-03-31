51ef1a95a09670a77265475c7ecda54d3696502a
Amanda French <amanda@amandafrench.net>
2014-03-31 19:43:36 -0400

    Install plugin email-log

---
+++ wp-content/plugins/email-log/include/class-email-log-list-table.php
@@ -0,0 +1,334 @@
+<?php
+/**
+ * Table to display Email Logs
+ *
+ * Based on Custom List Table Example by Matt Van Andel
+ *
+ * @package Email Log
+ * @author Sudar
+ */
+class Email_Log_List_Table extends WP_List_Table {
+
+    /**
+     * Set up a constructor that references the parent constructor. We
+     * use the parent reference to set some default configs.
+     */
+    function __construct(){
+        global $status, $page;
+
+        //Set parent defaults
+        parent::__construct( array(
+            'singular'  => 'email-log',     //singular name of the listed records
+            'plural'    => 'email-logs',    //plural name of the listed records
+            'ajax'      => false        //does this table support ajax?
+        ) );
+    }
+
+
+	/**
+	 * Add extra markup in the toolbars before or after the list
+	 * @param string $which, helps you decide if you add the markup after (bottom) or before (top) the list
+	 */
+	function extra_tablenav( $which ) {
+		if ( $which == "top" ){
+			//The code that goes before the table is here
+            echo '<span id = "el-pro-msg">';
+            _e('More fields are available in Pro addon. ', 'email-log');
+            echo '<a href = "http://sudarmuthu.com/out/buy-email-log-more-fields-addon" style = "color:red">';
+            _e('Buy Now', 'email-log');
+            echo '</a>';
+            echo '</span>';
+		}
+
+		if ( $which == "bottom" ){
+			//The code that goes after the table is there
+            echo '<p>&nbsp;</p>';
+            echo '<p>&nbsp;</p>';
+
+            echo '<p>';
+            _e('The following are the list of pro addons that are currently available for purchase.', 'email-log');
+            echo '</p>';
+
+            echo '<ul style="list-style:disc; padding-left:35px">';
+
+            echo '<li>';
+            echo '<strong>', __('Email Log - Forward Email', 'email-log'), '</strong>', ' - ';
+            echo __('This addon allows you to send a copy of all emails send from WordPress to another email address', 'email-log');
+            echo ' <a href = "http://sudarmuthu.com/wordpress/email-log/pro-addons#forward-email-addon">', __('More Info', 'email-log'), '</a>.';
+            echo ' <a href = "http://sudarmuthu.com/out/buy-email-log-forward-email-addon">', __('Buy now', 'email-log'), '</a>';
+            echo '</li>';
+
+            echo '<li>';
+            echo '<strong>', __('Email Log - More fields', 'email-log'), '</strong>', ' - ';
+            echo __('Adds more fields (From, CC, BCC, Reply To, Attachment) to the logs page.', 'email-log');
+            echo ' <a href = "http://sudarmuthu.com/wordpress/email-log/pro-addons#more-fields-addon">', __('More Info', 'email-log'), '</a>.';
+            echo ' <a href = "http://sudarmuthu.com/out/buy-email-log-more-fields-addon">', __('Buy now', 'email-log'), '</a>';
+            echo '</li>';
+
+            echo '</ul>';
+		}
+	}
+
+    /** ************************************************************************
+     * REQUIRED! This method dictates the table's columns and titles. This should
+     * return an array where the key is the column slug (and class) and the value
+     * is the column's title text. If you need a checkbox for bulk actions, refer
+     * to the $columns array below.
+     *
+     * The 'cb' column is treated differently than the rest. If including a checkbox
+     * column in your table you must create a column_cb() method. If you don't need
+     * bulk actions or checkboxes, simply leave the 'cb' entry out of your array.
+     *
+     * @see WP_List_Table::::single_row_columns()
+     * @return array An associative array containing column information: 'slugs'=>'Visible Titles'
+     **************************************************************************/
+    function get_columns(){
+        $columns = array(
+            'cb'        => '<input type="checkbox" />', //Render a checkbox instead of text
+            'sent_date' => __('Sent at', 'email-log'),
+            'to'        => __('To', 'email-log'),
+            'subject'   => __('Subject', 'email-log')
+        );
+
+        return apply_filters( EmailLog::HOOK_LOG_COLUMNS, $columns );
+    }
+
+    /** ************************************************************************
+     * Optional. If you want one or more columns to be sortable (ASC/DESC toggle),
+     * you will need to register it here. This should return an array where the
+     * key is the column that needs to be sortable, and the value is db column to
+     * sort by. Often, the key and value will be the same, but this is not always
+     * the case (as the value is a column name from the database, not the list table).
+     *
+     * This method merely defines which columns should be sortable and makes them
+     * clickable - it does not handle the actual sorting. You still need to detect
+     * the ORDERBY and ORDER querystring variables within prepare_items() and sort
+     * your data accordingly (usually by modifying your query).
+     *
+     * @return array An associative array containing all the columns that should be sortable: 'slugs'=>array('data_values',bool)
+     **************************************************************************/
+    function get_sortable_columns() {
+        $sortable_columns = array(
+            'sent_date'   => array('sent_date',TRUE),     //true means it's already sorted
+            'to'          => array('to_email',FALSE),
+            'subject'     => array('subject',FALSE)
+        );
+        return $sortable_columns;
+    }
+
+    /** ************************************************************************
+     * Recommended. This method is called when the parent class can't find a method
+     * specifically build for a given column. Generally, it's recommended to include
+     * one method for each column you want to render, keeping your package class
+     * neat and organized. For example, if the class needs to process a column
+     * named 'title', it would first see if a method named $this->column_title()
+     * exists - if it does, that method will be used. If it doesn't, this one will
+     * be used. Generally, you should try to use custom column methods as much as
+     * possible.
+     *
+     * Since we have defined a column_title() method later on, this method doesn't
+     * need to concern itself with any column with a name of 'title'. Instead, it
+     * needs to handle everything else.
+     *
+     * For more detailed insight into how columns are handled, take a look at
+     * WP_List_Table::single_row_columns()
+     *
+     * @param array $item A singular item (one full row's worth of data)
+     * @param array $column_name The name/slug of the column to be processed
+     * @return string Text or HTML to be placed inside the column <td>
+     **************************************************************************/
+    function column_default( $item, $column_name ){
+        do_action( EmailLog::HOOK_LOG_DISPLAY_COLUMNS, $column_name, $item );
+    }
+
+    /** ************************************************************************
+     * Recommended. This is a custom column method and is responsible for what
+     * is rendered in any column with a name/slug of 'title'. Every time the class
+     * needs to render a column, it first looks for a method named
+     * column_{$column_title} - if it exists, that method is run. If it doesn't
+     * exist, column_default() is called instead.
+     *
+     * This example also illustrates how to implement rollover actions. Actions
+     * should be an associative array formatted as 'slug'=>'link html' - and you
+     * will need to generate the URLs yourself. You could even ensure the links
+     *
+     *
+     * @see WP_List_Table::::single_row_columns()
+     * @param array $item A singular item (one full row's worth of data)
+     * @return string Text to be placed inside the column <td>
+     **************************************************************************/
+    function column_sent_date($item){
+
+        //Build row actions
+        $actions = array(
+            'delete' => sprintf( '<a href="?page=%s&action=%s&%s=%s&%s=%s">%s</a>',
+                                $_REQUEST['page'],
+                                'delete',
+                                $this->_args['singular'],
+                                $item->id,
+                                EmailLog::DELETE_LOG_NONCE_FIELD,
+                                wp_create_nonce( EmailLog::DELETE_LOG_ACTION ),
+                                __( 'Delete', 'email-log' )
+                        ),
+        );
+
+        $email_date = mysql2date(sprintf(__('%s @ %s', 'email-log'), get_option('date_format'), get_option('time_format')), $item->sent_date);
+
+        return sprintf('%1$s <span style="color:silver">[<a href="#" class="email_content" id="email_content_%2$s">%3$s</a>] (id:%4$s)</span>%5$s',
+            /*$1%s*/ $email_date,
+            /*$2%s*/ $item->id,
+            /*$3%s*/ __('View Content', 'email-log' ),
+            /*$4%s*/ $item->id,
+            /*$5%s*/ $this->row_actions($actions)
+        );
+    }
+
+    /**
+     * To field
+     */
+    function column_to( $item ) {
+        return stripslashes( $item->to_email );
+    }
+
+    /**
+     * Subject field
+     */
+    function column_subject( $item ) {
+        return stripslashes( $item->subject );
+    }
+
+    /** ************************************************************************
+     * REQUIRED if displaying checkboxes or using bulk actions! The 'cb' column
+     * is given special treatment when columns are processed. It ALWAYS needs to
+     * have it's own method.
+     *
+     * @see WP_List_Table::::single_row_columns()
+     * @param array $item A singular item (one full row's worth of data)
+     * @return string Text to be placed inside the column <td> (movie title only)
+     **************************************************************************/
+    function column_cb($item){
+        return sprintf(
+            '<input type="checkbox" name="%1$s[]" value="%2$s" />',
+            /*$1%s*/ $this->_args['singular'],
+            /*$2%s*/ $item->id
+        );
+    }
+
+    /**
+     * Specify the list of bulk actions
+     *
+     * @return array An associative array containing all the bulk actions: 'slugs'=>'Visible Titles'
+     */
+    function get_bulk_actions() {
+        $actions = array(
+            'delete'     => __( 'Delete', 'email-log' ),
+            'delete-all' => __( 'Delete All Logs', 'email-log' )
+        );
+        return $actions;
+    }
+
+    /**
+     * Handle bulk actions
+     *
+     * @see $this->prepare_items()
+     */
+    function process_bulk_action() {
+        global $wpdb;
+        global $EmailLog;
+
+        if( 'delete' === $this->current_action() ) {
+            // delete a list of logs by id
+
+            $nouce = $_REQUEST[EmailLog::DELETE_LOG_NONCE_FIELD ];
+            if ( wp_verify_nonce( $nouce, EmailLog::DELETE_LOG_ACTION ) ) {
+
+                $ids =  $_GET[$this->_args['singular']];
+
+                if ( is_array( $ids ) ) {
+                    $selected_ids = implode( ',', $ids );
+                } else {
+                    $selected_ids = $ids;
+                }
+
+                // Can't use wpdb->prepare for the below query. If used it results in this bug
+                // https://github.com/sudar/email-log/issues/13
+
+                $selected_ids = esc_sql( $selected_ids );
+
+                $table_name = $wpdb->prefix . EmailLog::TABLE_NAME;
+                $EmailLog->logs_deleted = $wpdb->query( "DELETE FROM $table_name where id IN ( $selected_ids )" );
+            } else {
+                wp_die( 'Cheating, Huh? ');
+            }
+        } else if( 'delete-all' === $this->current_action() ) {
+            // delete all logs
+
+            $nouce = $_REQUEST[EmailLog::DELETE_LOG_NONCE_FIELD ];
+            if ( wp_verify_nonce( $nouce, EmailLog::DELETE_LOG_ACTION ) ) {
+                $table_name = $wpdb->prefix . EmailLog::TABLE_NAME;
+                $EmailLog->logs_deleted = $wpdb->query( "DELETE FROM $table_name" );
+            } else {
+                wp_die( 'Cheating, Huh? ');
+            }
+        }
+    }
+
+    /**
+     * Prepare data for display.
+     */
+    function prepare_items() {
+        global $wpdb;
+
+        $table_name = $wpdb->prefix . EmailLog::TABLE_NAME;
+        $this->_column_headers = $this->get_column_info();
+
+        // Handle bulk actions
+        $this->process_bulk_action();
+
+        // get current page number
+        $current_page = $this->get_pagenum();
+
+        $query = "SELECT * FROM " . $table_name;
+
+        if ( isset( $_GET['s'] ) ) {
+            $search_term = trim( esc_sql( $_GET['s'] ) );
+            $query .= " WHERE to_email LIKE '%$search_term%' OR subject LIKE '%$search_term%' ";
+        }
+
+        // Ordering parameters
+	    $orderby = !empty( $_GET["orderby"] ) ? esc_sql( $_GET["orderby"] ) : 'sent_date';
+	    $order   = !empty( $_GET["order"] ) ? esc_sql( $_GET["order"] ) : 'DESC';
+
+        if(!empty($orderby) & !empty($order)) {
+            $query .= ' ORDER BY ' . $orderby . ' ' . $order;
+        }
+
+        // Pagination parameters
+        $total_items = $wpdb->query( $query ); //return the total number of affected rows
+
+        //adjust the query to take pagination into account
+        $per_page = EmailLog::get_per_page();
+	    if( !empty( $current_page ) && !empty( $per_page ) ) {
+		    $offset = ($current_page-1) * $per_page;
+            $query .= ' LIMIT ' . (int)$offset . ',' . (int)$per_page;
+	    }
+
+        // Fetch the items
+        $this->items = $wpdb->get_results( $query );
+
+        // register pagination options & calculations.
+        $this->set_pagination_args( array(
+            'total_items' => $total_items,
+            'per_page'    => $per_page,
+            'total_pages' => ceil( $total_items/$per_page )
+        ) );
+    }
+
+    /**
+     * If no items are found
+     */
+    function no_items() {
+        _e('Your email log is empty', 'email-log');
+    }
+}
+?>
---
+++ wp-content/plugins/email-log/readme.txt
@@ -0,0 +1,219 @@
+=== Email Log ===
+Contributors: sudar  
+Tags: email, wpmu, wordpress-mu, log  
+Requires at least: 3.3  
+Tested up to: 3.8.1  
+Stable tag: 1.7  
+
+Logs every email sent through WordPress. Works with WordPress Multisite as well
+
+== Description ==
+
+Logs every email sent through WordPress and provides a UI where you can view them.
+
+### Viewing logged emails
+
+The logged emails will be stored in a separate table and can be viewed from the admin interface. While viewing the logs, the emails can be filtered or sorted based on the date, email, subject etc.
+
+### Deleting logged emails
+
+In the admin interface, all the logged emails can be delete in bulk or can also be selectively deleted based on date, email and subject.
+
+### Forward email (Pro addon)
+
+You can [buy the Forward email pro addon](http://sudarmuthu.com/wordpress/email-log/pro-addons#forward-email-addon), which allows you to send a copy of all the emails send from WordPress, to another email address. The addon allows you to choose whether you want to forward through to, cc or bcc fields. This can be extremely useful when you want to debug by analyzing the emails that are sent from WordPress. The cost of the addon is $15 and you can buy it through [paypal](http://sudarmuthu.com/out/buy-email-log-forward-email-addon).
+
+### More Fields (Pro addon)
+
+You can [buy the More Fields pro addon](http://sudarmuthu.com/wordpress/email-log/pro-addons#more-fields-addon), which shows additional fields in the email log page. The following are the additional fields that are added by this addon.
+
+- From
+- CC
+- BCC
+- Reply To
+- Attachment
+
+The cost of the addon is $15 and you can buy it through [paypal](http://sudarmuthu.com/out/buy-email-log-more-fields-addon).
+
+
+### Cleaning up db on uninstall
+
+As [recommended by Ozh][1], the Plugin has an uninstall hook which will clean up the database when the Plugin is uninstalled.
+
+ [1]: http://sudarmuthu.com/blog/2009/10/07/lessons-from-wordpress-plugin-competition.html
+
+### Development
+
+The development of the Plugin happens over at [github](http://github.com/sudar/email-log). If you want to contribute to the Plugin, [fork the project at github](http://github.com/sudar/email-log) and send me a pull request.
+
+If you are not familiar with either git or Github then refer to this [guide to see how fork and send pull request](http://sudarmuthu.com/blog/contributing-to-project-hosted-in-github).
+
+If you are looking for ideas, then you can start with one of the following TODO items :)
+
+### TODO for Future releases
+
+The following are the features that I am thinking of adding to the Plugin, when I get some free time. If you have any feature request or want to increase the priority of a particular feature, then let me know.
+
+- Add option to automatically delete the logs periodically
+- Add the ability to resend the emails
+- <strike>Make it MU compatible</strike>. Done in v1.7
+
+### Support
+
+- If you have found a bug/issue or have a feature request, then post them in [github issues](https://github.com/sudar/email-log/issues)
+- If you have a question about usage or need help to troubleshoot, then post in WordPress forums or leave a comment in [Plugins's home page][1]
+- If you like the Plugin, then kindly leave a review/feedback at [WordPress repo page][7].
+- If you find this Plugin useful or and wanted to say thank you, then there are ways to [make me happy](http://sudarmuthu.com/if-you-wanna-thank-me) :) and I would really appreciate if you can do one of those.
+- If anything else, then contact me in [twitter][2].
+
+### Stay updated
+
+I would be posting updates about this Plugin in my [blog][3] and in [Twitter][2]. If you want to be informed when new version of this Plugin is released, then you can either subscribe to this [blog's RSS feed][4] or [follow me in Twitter][2].
+
+You can also checkout some of the [other Plugins that I have released][5].
+
+ [1]: http://sudarmuthu.com/wordpress/email-log
+ [2]: http://twitter.com/sudarmuthu
+ [3]: http://sudarmuthu.com/blog
+ [4]: http://sudarmuthu.com/feed
+ [5]: http://sudarmuthu.com/wordpress
+ [7]: http://wordpress.org/extend/plugins/email-log
+
+== Translation ==
+
+The Plugin currently has translations for the following languages.
+
+*   German (Thanks Frank)
+*   Lithuanian (Thanks  Vincent G)
+*   Dutch (Thanks Zjan Preijde)
+
+The pot file is available with the Plugin. If you are willing to do translation for the Plugin, use the pot file to create the .po files for your language and let me know. I will add it to the Plugin after giving credit to you.
+
+== Installation ==
+
+### Normal WordPress installations
+
+Extract the zip file and just drop the contents in the wp-content/plugins/ directory of your WordPress installation and then activate the Plugin from Plugins page.
+
+== Screenshots ==
+
+1. The above screenshot shows how the logged emails will be displayed by the Plugin
+
+2. This screenshot shows how you can configure the email display screen. You can choose the fields and the number of emails per page
+
+3. This screenshot shows the additional fields that will be added by the [more fields](http://sudarmuthu.com/wordpress/email-log/pro-addons#more-fields-addon) addon
+
+4. The above screenshot shows how the logged emails will be displayed by the Plugin after you install the [more fields](http://sudarmuthu.com/wordpress/email-log/pro-addons#more-fields-addon) addon
+
+5. This screenshot shows the settings page of [forward email](http://sudarmuthu.com/wordpress/email-log/pro-addons#forward-email-addon) addon
+
+== Readme Generator ==
+
+This Readme file was generated using <a href = 'http://sudarmuthu.com/wordpress/wp-readme'>wp-readme</a>, which generates readme files for WordPress Plugins.
+== Changelog ==
+
+= v1.7  (2014-03-29) - (Dev time: 2.5 hours) =
+- Fix: Fix whitespace
+- New: Add support for WordPress Multisite (issue #18)
+- New: Add ability to delete all logs at once (issue #19)
+
+= v1.6.2  (2014-01-27) - (Dev time: 0.5 hours) =
+- Fix: Fix unexpected output while activating the plugin
+
+= v1.6.1  (2013-12-17) - (Dev time: 0.5 hours) =
+- Fix: Change `prepare_items` function so that it adheres to strict mode
+- Fix: Remove `screen_icon` function call which is not used in WordPress 3.8
+- New: Compatible with WordPress 3.8
+
+= v1.6  (2013-12-08) - (Dev time: 0.5 hours) =
+- New: Add a link to view the content of the email in the log screen
+
+= v1.5.4  (2013-09-21) - (Dev time: 0.5 hours) =
+- Fix issue in searching non-english characters
+- Add addon screenshots
+
+= v1.5.3 (2013-09-14) - (Dev time: 0.5 hours) =
+- Fix issue in bulk deleting logs
+
+= v1.5.2 (2013-09-13) - (Dev time: 0.5 hours) =
+- Add the ability to override the fields displayed in the log page
+- Add support for "More Fields" addon
+
+= v1.5.1 (2013-09-09) - (Dev time: 0.5 hours) =
+- Correct the upgrade file include path. Issue #7
+- Fix undfined notice error. Issue #8
+- Update screenshots. Issue #6
+
+= v1.5 (2013-09-09) - (Dev time: 10 hours) =
+- Rewrote Admin interface using native tables
+
+= v1.1 (2013-04-27) - (Dev time: 0.5 hour)  =
+- Added more documentation
+
+= v1.0 (2013-04-17) - (Dev time: 0.5 hour)  =
+- Added support for buying pro addons
+
+= v0.9.3 (2013-04-01) - (Dev time: 0.5 hour)  =
+- Moved table name into a separate constants file
+
+= v0.9.2 (2013-03-14) - (Dev time: 0.5 hour)  =
+- Added support for filters which can be used while logging emails
+
+= v0.9.1 (2013-01-08) - (Dev time: 0.5 hour)  =
+- Moved the menu under tools (Thanks samuelaguilera)
+
+= v0.9(2013-01-08) - (Dev time: 1 hour)  =
+- Use blog date/time for send date instead of server time
+- Handle cases where the headers send is an array
+
+= v0.8.1 (2012-07-23) (Dev time: 0.5 hour) =
+- Reworded most error messages and fixed lot of typos
+
+= v0.8 (2012-07-12) (Dev time: 1 hour) =
+- Fixed undefined notices - http://wordpress.org/support/topic/plugin-email-log-notices-undefined-indices
+- Added Dutch translations
+
+= v0.7 (2012-06-23) (Dev time: 1 hour) =
+- Changed Timestamp(n) MySQL datatype to Timestamp (now compatible with MySQL 5.5+)
+- Added the ability to bulk delete checkboxes
+
+= v0.6 (2012-04-29) (Dev time: 2 hours) =
+- Added option to delete individual email logs
+- Moved pages per screen option to Screen options panel
+- Added information to the screen help tab
+- Added Lithuanian translations
+
+= v0.5 (2012-01-01) =
+- Fixed a deprecation notice
+
+= v0.4 (2010-01-02) =
+- Added German translation (Thanks Frank)
+
+= v0.3 (2009-10-19) =
+- Added compatibility for MySQL 4 (Thanks Frank)
+
+= v0.2 (2009-10-15) =
+- Added compatibility for MySQL 4
+
+= v0.1 (2009-10-08) =
+- Initial Release
+
+== Upgrade Notice ==
+
+= 1.6 =
+Ability to view content of the email
+
+= 1.5.4 =
+Fixed issue in searching for non-english characters
+
+= 1.5.3 =
+Fix issue in bulk deleting logs
+
+= 1.5 =
+Rewrote Admin interface using native tables
+
+= 1.0 =
+Added support for buying pro addons
+
+= 0.9.2 =
+Added filters for more customizing
