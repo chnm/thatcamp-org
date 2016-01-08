4233c17c3c76f0db353273e0cd162bf4417c04b3
Boone B Gorges <boonebgorges@gmail.com>
2016-01-08 15:06:48 -0600

    Add user to main site when activating through main site.
    This ensures that users have the necessary permission to visit
    thatcamp.org/wp-admin/profile.php when not registering through a subsite.
    
    See #41.

---
+++ wp-content/mu-plugins/thatcamp-add-user-to-site.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * For users who register via BuddyPress (/signup/), ensure that they are members of the main site so that they
+ * are able to edit their profiles.
+ */
+function thatcamp_add_user_to_main_site_on_activation( $user_id ) {
+	add_user_to_site( bp_get_root_blog_id(), $user_id, 'subscriber' );
+}
+add_action( 'bp_core_activated_user', 'thatcamp_add_user_to_main_site_on_activation' );
