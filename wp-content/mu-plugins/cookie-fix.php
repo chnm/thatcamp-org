4a8bac51f68a1ae7f182e3a379e183a7af525f92
Boone B Gorges <boonebgorges@gmail.com>
2016-03-29 09:55:51 -0500

    Suppress `setcookie()` errors in mu-plugins/cookie-fix.php.
    Mostly for wp-cli.

--- wp-content/mu-plugins/cookie-fix.php
+++ wp-content/mu-plugins/cookie-fix.php
@@ -8,7 +8,7 @@ License: GPL2
 */
 
 function set_wp_test_cookie() {
-	setcookie(TEST_COOKIE, 'WP Cookie check', 0, COOKIEPATH, COOKIE_DOMAIN);
+	@setcookie(TEST_COOKIE, 'WP Cookie check', 0, COOKIEPATH, COOKIE_DOMAIN);
 	if ( SITECOOKIEPATH != COOKIEPATH )
 		setcookie(TEST_COOKIE, 'WP Cookie check', 0, SITECOOKIEPATH, COOKIE_DOMAIN);
 }
