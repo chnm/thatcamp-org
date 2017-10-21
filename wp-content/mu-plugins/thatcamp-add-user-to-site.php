294396792d277546c3f7d6172711aca9472d3de2
Amanda French <amanda@amandafrench.net>
2017-10-21 09:09:06 -0400

    Adds all new users as subscribers to main blog so they can edit their profile

--- wp-content/mu-plugins/thatcamp-add-user-to-site.php
+++ wp-content/mu-plugins/thatcamp-add-user-to-site.php
@@ -8,3 +8,12 @@ function thatcamp_add_user_to_main_site_on_activation( $user_id ) {
 	add_user_to_blog( bp_get_root_blog_id(), $user_id, 'subscriber' );
 }
 add_action( 'bp_core_activated_user', 'thatcamp_add_user_to_main_site_on_activation' );
+
+/** For users who register via subsites, ensure that they are members of the main site so that they
+ * are able to edit their profiles.
+ */
+
+function thatcamp_add_user_to_main_site_on_registration( $user_id ) {
+	add_user_to_blog( bp_get_root_blog_id(), $user_id, 'subscriber' );
+}
+add_action( 'user_register', 'thatcamp_add_user_to_main_site_on_registration' );
