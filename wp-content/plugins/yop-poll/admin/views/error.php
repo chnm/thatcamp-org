42c73900e485e1b4cd579a587809b911ff5aee8e
amandafrench <amanda@amandafrench.net>
2019-12-11 17:03:09 -0500

    Uninstalls plugin yop-poll

--- wp-content/plugins/yop-poll/admin/views/error.php
+++
@@ -1,9 +0,0 @@
-<section id="yop-poll-errors">
-    <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" align="center">
-            <h2>
-                <?php echo $error; ?>
-            </h2>
-        </div>
-    </div>
-</section>
--- wp-content/plugins/yop-poll/uninstall.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-    die;
-}
-require_once( 'admin/inc/dbschema.php' );
-require_once( 'admin/inc/maintenance.php' );
-require_once( 'admin/inc/capabilities.php' );
-$yop_poll_maintenance = new YOP_POLL_Maintenance;
-$yop_poll_maintenance->uninstall();
